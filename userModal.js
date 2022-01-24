9b7bf9290babb795fffe3e36d3c45a826058f3c6
Mahmudul Hasan <shuvonovo@gmail.com>
2022-01-24 21:24:32 +0600

    hash to password

--- userModal.js
+++ userModal.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -13,6 +14,12 @@ const userSchema = new mongoose.Schema({
         type: String,
     }
 })
-
+userSchema.pre('save', async function (next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 10)
+    }
+    next()
+})
 const User = mongoose.model('User', userSchema)
 module.exports = User
